655a572507d9e9029e326a1ed09f064381cecb23
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-12-31 06:46:48 +0300

    put files inside right directory

---
+++ 0x00-python_variable_annotations/examples/1-ex.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'using type aliases'
+
+type Vector = List[float]
+
+def scale(scalar: float, vector: Vector) -> Vector:
+    return [ scalar * num for num in vector ]
+
+new_vector = scale(2.0, [1.0, -4.2, 5.4 ])
+print(new_vector)
