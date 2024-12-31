922c92e4958250976cae3b1f6962fcdde1797a2f
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-12-31 10:50:51 +0300

    function that puts strings together

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'holds function that takes str1 and str2 and puts them together'
+
+
+def concat(str1: str, str2: str) -> str:
+    'function that puts strings together'
+    return str1 + str2
