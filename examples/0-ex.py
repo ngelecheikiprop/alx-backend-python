7e3c7b55e9173eec17a7141cb06859ad2ec2e6b5
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-12-31 03:07:14 +0000

    first example on anotations

---
+++ examples/0-ex.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+def surface_area_of_cube(edge_length: float) -> str:
+    return f"The surface area of the cube is {6 * edge_length ** 2}."
+
+print(surface_area_of_cube("2"))
