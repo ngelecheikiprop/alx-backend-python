3233c0e438ddd9246483d0d58b5b34630d9278a7
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-12-31 08:48:13 +0000

    number 5 changed to older understanding

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,10 +2,10 @@
 'holds function that return sums of float'
 
 
-def sum_list(input_list: list[float]) -> float:
+from typing import List
+def sum_list(input_list: List[float]) -> float:
     'takes list of float and returns sum as float'
     sum: float = 0.0
     for num in input_list:
         sum += num
     return sum
-
