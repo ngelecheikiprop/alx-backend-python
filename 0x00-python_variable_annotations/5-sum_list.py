c29a512f0475871e01077f7e3972beedddfcb3e4
ngelecheikiprop <ngelecheikiprop@gmail.com>
2025-01-03 06:51:15 +0000

    added 2 lines for style correction

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 'holds function that return sums of float'
-
-
 from typing import List
+
+
 def sum_list(input_list: List[float]) -> float:
     'takes list of float and returns sum as float'
     sum: float = 0.0
